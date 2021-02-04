f3f6bd5376320e9b156df54c4cbfa23827d70720
BorisRendon <boris.rendon@ufm.edu>
2021-02-03 22:58:14 -0600

    upload notebooks and sql

---
+++ Practica01/queries/unconfirmed_transactions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE test_time_series_covid (
+	`index` BIGINT, 
+	`Province/State` TEXT, 
+	`Country/Region` TEXT, 
+	`Lat` FLOAT(53), 
+	`Long` FLOAT(53), 
+	fecha TEXT, 
+	confirmed BIGINT
+)
+
